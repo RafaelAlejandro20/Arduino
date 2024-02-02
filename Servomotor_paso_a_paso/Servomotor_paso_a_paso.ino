45b4c87159e2b9e64974518d9c4f1a2b6734414d
Rafael Alejandro Barajas Urrutia <82453734+RafaelAlejandro20@users.noreply.github.com>
2024-02-01 19:29:44 -0600

    Update Servomotor_paso_a_paso.ino

--- Servomotor_paso_a_paso/Servomotor_paso_a_paso.ino
+++ Servomotor_paso_a_paso/Servomotor_paso_a_paso.ino
@@ -1,6 +1,6 @@
 #include <Stepper.h>
 int stepsPerRevolution = 2048;
-int modSpeed = 10;
+int modSpeed = 13;
 Stepper MyStepper(stepsPerRevolution,8,10,9,11);
 int dt = 1500;
 void setup() {
@@ -10,6 +10,6 @@ void setup() {
 void loop() {
   MyStepper.step(stepsPerRevolution);
   delay(dt);
-  MyStepper.step(stepsPerRevolution);
+  MyStepper.step(-stepsPerRevolution);
   delay(dt);
 }
